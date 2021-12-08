b96c8c577f8497f49fe5b3f93fcd5152894d97d4
Vladimir Rebo <vladimir.rebo@ffins.kz>
2021-12-08 14:30:29 +0600

    changed method type

--- GitLabWebhook/Controllers/GitLabWebhookController.cs
+++ GitLabWebhook/Controllers/GitLabWebhookController.cs
@@ -16,7 +16,7 @@ public class GitLabWebhookController : ControllerBase
         _bot = bot;
     }
     
-    [HttpGet, Route("")]
+    [HttpPost, Route("")]
     public async Task<IActionResult> Get([FromBody] EventRequest eventRequest)
     {
         _logger.LogInformation("Request is come");
