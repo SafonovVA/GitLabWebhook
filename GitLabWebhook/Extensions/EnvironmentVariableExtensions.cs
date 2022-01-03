2e2dcff94f8019401956a8d219e3656279d0930d
Vladimir Rebo <learn@contoso.com>
2022-01-03 18:07:15 +0600

    rename variable

--- GitLabWebhook/Extensions/EnvironmentVariableExtensions.cs
+++ GitLabWebhook/Extensions/EnvironmentVariableExtensions.cs
@@ -5,12 +5,12 @@ public static class EnvironmentVariableExtensions
     public static string GetConnectionStringFromEnvironment(
         this IConfiguration configuration, string name)
     {
-        var variable = Environment.GetEnvironmentVariable(name);
-        if (string.IsNullOrEmpty(variable))
+        var value = Environment.GetEnvironmentVariable(name);
+        if (string.IsNullOrEmpty(value))
         {
-            throw new ArgumentException();
+            throw new ArgumentException($"Environment variable '{name}' value is '{value}' (is null or empty)");
         }
 
-        return variable;
+        return value;
     }
 }
